1c29142ec470bad12eb335936c9afeb4d4674adf
Harshit Dawar <31154755+HarshitDawar55@users.noreply.github.com>
2018-04-19 19:39:36 +0530

    c++ exception handling program
    c++ exception handling program for division by 0

---
+++ exceptionp.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Enter Two number"<<endl;
+int a,b,div=1;
+cin>>a>>b;
+try{
+if(b==0){
+	throw"error";
+}
+div=a/b;
+cout<<"answer= "<<div<<endl;
+}
+catch(const char *msg){
+	cout<<"Cannot divide by 0"<<endl;
+}
+return 0;
+}
